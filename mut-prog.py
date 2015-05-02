ac326c8ed1dfc7b0ddbaf94f9bf1a9fc55c19d42
darioush <darioush.jalali@gmail.com>
2015-05-02 12:17:18 -0700

    1st attempt progress muts

--- mut-prog.py
+++ mut-prog.py
@@ -18,9 +18,11 @@ def main():
         executing_muts = [int(match.group(1)) for match in
                 [re.match(r'"Mutant-(\d+)" .*', line) for line in mutants]
                 if match is not None]
-        print executing_muts
 
-    [info(line) for line in mutations]
+        return {'pid': pid, 'total': total_muts, 'running': executing_muts}
+
+    infos = sorted([info(line) for line in mutations], key=lambda x: x['pid'])
+    print infos
 
 #5865 ant-launcher.jar -f /home/ec2-user/defects4j/framework/build-scripts/Math/Math.build.xml -Dscript.dir=/home/ec2-user/defects4j/framework -Dbasedir=/tmp/worker.ip-172-31-25-99.5851/checkout -Dmajor.exclude=/tmp/worker.ip-172-31-25-99.5851/checkout/exclude.txt -Dmajor.kill.log=/tmp/worker.ip-172-31-25-99.5851/checkout/kill.csv -logfile /tmp/worker.ip-172-31-25-99.5851/checkout/.mutation.log -Dtest.entry.class=org.apache.commons.math3.analysis.function.SincTest -Dtest.entry.method=testDerivativeShortcut mutation.test
 
